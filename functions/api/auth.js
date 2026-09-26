40046ddc96f3d1f7e01335f2802f347e08d07664
Peter Kanyuira <p.kanyuira@gmail.com>
2026-09-26 13:39:24 +0300

    Update GitHub OAuth redirect URI

--- functions/api/auth.js
+++ functions/api/auth.js
@@ -1,6 +1,6 @@
 export async function onRequest(context) {
   const clientId = context.env.OAUTH_GITHUB_CLIENT_ID;
-  const redirectUri = `https://elyon-elementaita-resort.pages.dev/api/callback`;
+  const redirectUri = `https://elyonelementaitaresort.com/api/callback`;
   
   const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=repo`;
   
